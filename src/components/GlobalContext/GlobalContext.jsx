e3d471d4a52bb00e33011cc82de71056b4c35c54
Mini-Sylar <minisylar3@gmail.com>
2023-06-22 20:32:37 +0000

    Add login button

--- src/components/GlobalContext/GlobalContext.jsx
+++ src/components/GlobalContext/GlobalContext.jsx
@@ -2,6 +2,7 @@ import { createContext, useContext } from "react";
 import useStore from "../../store/products";
 import useAuth from "../../store/auth";
 import useModal from "../../store/modal";
+import useOrders from "../../store/orders";
 
 const globalContext = createContext();
 
@@ -11,8 +12,9 @@ const GlobalContext = ({ children }) => {
   const store = useStore();
   const auth = useAuth();
   const modal = useModal();
+  const orders = useOrders();
   return (
-    <globalContext.Provider value={{ store, auth, modal }}>
+    <globalContext.Provider value={{ store, auth, modal, orders }}>
       {children}
     </globalContext.Provider>
   );
--- src/components/NavBar/Account/Account.jsx
+++ src/components/NavBar/Account/Account.jsx
@@ -2,11 +2,17 @@ import { FaShoppingCart } from "react-icons/fa";
 
 import { useGlobalContext } from "@/components/GlobalContext/GlobalContext";
 import { Link } from "react-router-dom";
+import "./Account.css";
 
 const Account = () => {
-  let { store } = useGlobalContext();
-  let { auth } = useGlobalContext();
+  // let { store } = useGlobalContext();
+  let { auth, store, modal } = useGlobalContext();
   const cartTotal = store.state.cartQuantity;
+
+  const handleShowModal = () => {
+    modal.openModal(false);
+  };
+
   return (
     <div className="account">
       <div className="cart">
@@ -23,6 +29,11 @@ const Account = () => {
             <span className="items-in-cart">{cartTotal}</span>
           </span>
         </Link>
+      </div>
+      <div className="login">
+        <button className="btn-rounded small-rounded" onClick={handleShowModal}>
+          Login
+        </button>
       </div>
     </div>
   );
