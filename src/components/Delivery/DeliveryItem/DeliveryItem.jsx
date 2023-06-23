19c552458d20506d901cbe0522bf986cffae6ca5
Mini-Sylar <minisylar3@gmail.com>
2023-06-23 08:27:54 +0000

    Style Delivery Items

--- src/components/Delivery/DeliveryItem/DeliveryItem.jsx
+++ src/components/Delivery/DeliveryItem/DeliveryItem.jsx
@@ -1,6 +1,18 @@
+import "./DeliveryItem.css";
+
 const DeliveryItem = ({ order }) => {
+  const date = new Date(order.expected_delivery_date);
+  const currentDate = new Date();
+  const formattedDate = date.toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+  const numberOfDays = Math.ceil(
+    (date.getTime() - currentDate.getTime()) / (1000 * 3600 * 24)
+  );
   return (
-    <div>
+    <div className="sub-container delivery-item-container">
       <div className="delivery-overview">
         <div className="delivery-summary">
           <div className="delivery-order-number">
@@ -21,7 +33,8 @@ const DeliveryItem = ({ order }) => {
           </div>
           <div className="delivery-date">
             <h3>Expected Completion</h3>
-            
+            <h4>{formattedDate}</h4>
+            <h4>{numberOfDays} day(s)</h4>
           </div>
         </div>
       </div>
